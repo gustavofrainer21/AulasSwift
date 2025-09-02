72426337232423654daa5baed418e753ac59ede4
Gustavo Frainer <guga.frainer1@gmail.com>
2025-09-02 20:16:45 -0300

    Exercício 1

--- Sources/Aula_2/main.swift
+++ Sources/Aula_2/main.swift
@@ -1,3 +1,18 @@
-import Foundation
+struct Livro {
+    var titulo: String
+    var autor: String 
+    var anoPublicado: Int
+    
+    func exibirDetalhes() {
+        print(self.titulo)
+        print(self.autor)
+        print(self.anoPublicado)
+        print()
+    }
+}
 
-print("Aula 2 de Swift!!")
+let livro1 = Livro(titulo: "Harry Potter", autor: "Tia Maria", anoPublicado: 2005)
+let livro2 = Livro(titulo: "Harry Potter 2", autor: "Tia Maria 2", anoPublicado: 2007)
+
+livro1.exibirDetalhes()
+livro2.exibirDetalhes()
