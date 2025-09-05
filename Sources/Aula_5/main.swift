4a7a1cd54b86197554c2c17562140a96f8891d95
Gustavo Frainer <guga.frainer1@gmail.com>
2025-09-05 18:45:25 +0000

    Aula 5 - Commit 2

--- Sources/Aula_5/main.swift
+++ Sources/Aula_5/main.swift
@@ -1,19 +1,14 @@
 import Foundation
 
 var meuCarro = CarroDeEntrega(placa: "FKG6057", velocidade: 0)
-var meuCarro2 = Veiculo(velocidade: 0)
 
 print("Rodada 1")
 meuCarro.acelerar()
 print(meuCarro.velocidade)
-
-meuCarro2.acelerar()
-print(meuCarro2.velocidade)
+print(meuCarro.obterLocalizacao())
 print()
 
 print("Rodada 2")
 meuCarro.acelerar()
 print(meuCarro.velocidade)
-
-meuCarro2.acelerar()
-print(meuCarro2.velocidade)
+print(meuCarro.obterLocalizacao())
